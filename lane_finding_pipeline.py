c7bdeeb6c313f64ea80b2ab6d57c44904ca27df4
atripathy <atripathy@yahoo-inc.com>
2017-06-29 21:05:21 -0700

    Adding some temp files

--- lane_finding_pipeline.py
+++ lane_finding_pipeline.py
@@ -238,6 +238,127 @@ img_birdeye, M, Minv = perspectiveTransform(cv2.cvtColor(img_undistorted, cv2.CO
 
 
 
+## Now do the sliding window fitting part
+def sliding_window_polyfit(birdeye_img):
+    """
+    input birdeye frame
+    """
+    height, width,_ = birdeye_img.shape
+    histogram = np.sum(birdeye_img[height/2:],axis=0)
+    # Find the peak of the left and right halves of the histogram
+    # These will be starting point of left and right lines
+    midpoint = np.int(histogram.shape[0]/2)
+    quarterpoint = np.int(midpoint//2)
+    # Previously the left/right base was the max of the left/right half of the histogram
+    # this changes it so that only a quarter of the histogram (directly to the left/right) is considered
+    leftx_base  = np.argmax(histogram[quarterpoint:midpoint])+quarterpoint
+    rightx_base = np.argmax(histogram[midpoint:midpoint+quarterpoint])+midpoint
+
+    # Choose the number of sliding windows
+    nwindows = 9
+    # Set height of windows
+    window_height = np.int(binary_warped.shape[0]/nwindows)
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = binary_warped.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+    # Set the width of the windows +/- margin
+    margin = 100
+    # Set minimum number of pixels found to recenter window
+    minpix = 50
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+    # Rectangle data for visualization
+    rectangle_data = []
+    for  window in range(nwindows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = birdeye_img.shape[0]-(window+1)*window_height
+        win_y_high = birdeye_img.shape[0]-window*window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+        rectangle_data.append((win_y_low, win_y_high, win_xleft_low, win_xleft_high, win_xright_low, win_xright_high))
+
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xleft_low) & (nonzerox < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) & (nonzerox >= win_xright_low) & (nonzerox < win_xright_high)).nonzero()[0]
+
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzerox[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzerox[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+    # Extract left and right line pixel positions
+    leftx = nonzerox[left_lane_inds]
+    lefty = nonzeroy[left_lane_inds]
+    rightx = nonzerox[right_lane_inds]
+    righty = nonzeroy[right_lane_inds]
+
+    if len(leftx) != 0:
+        left_fit = np.polyfit(lefty, leftx, 2)
+    if len(rightx) != 0:
+        right_fit = np.polyfit(righty, rightx, 2)
+
+    visualization_data = (rectangle_data, histogram)
+    return left_fit, right_fit, left_lane_inds, right_lane_inds, visualization_data
+print("############## Sliding Window Polyfit#############")
+
+exampleImg = cv2.imread('test_images/test2.jpg')
+exampleImg = cv2.cvtColor(exampleImg, cv2.COLOR_BGR2RGB)
+
+exampleImg_undistorted = undistort(exampleImg,mtx,dist)
+
+exampleImg_binary = binariseImage(exampleImg_undistorted,False)
+_, M, Minv = perspectiveTransform(cv2.cvtColor(exampleImg_binary, cv2.COLOR_BGR2RGB), True)
+
+
+
+left_fit, right_fit, left_lane_inds, right_lane_inds, visualization_data = sliding_window_polyfit(exampleImg_bin)
+
+h = exampleImg.shape[0]
+left_fit_x_int = left_fit[0]*h**2 + left_fit[1]*h + left_fit[2]
+right_fit_x_int = right_fit[0]*h**2 + right_fit[1]*h + right_fit[2]
+#print('fit x-intercepts:', left_fit_x_int, right_fit_x_int)
+
+rectangles = visualization_data[0]
+histogram = visualization_data[1]
+
+
+# Create an output image to draw on and  visualize the result
+out_img = np.uint8(np.dstack((exampleImg_binary, exampleImg_binary, exampleImg_binary))*255)
+# Generate x and y values for plotting
+ploty = np.linspace(0, exampleImg_binary.shape[0]-1, exampleImg_binary.shape[0] )
+left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+for rect in rectangles:
+# Draw the windows on the visualization image
+    cv2.rectangle(out_img,(rect[2],rect[0]),(rect[3],rect[1]),(0,255,0), 2)
+    cv2.rectangle(out_img,(rect[4],rect[0]),(rect[5],rect[1]),(0,255,0), 2)
+# Identify the x and y positions of all nonzero pixels in the image
+nonzero = exampleImg_binary.nonzero()
+nonzeroy = np.array(nonzero[0])
+nonzerox = np.array(nonzero[1])
+out_img[nonzeroy[left_lane_inds], nonzerox[left_lane_inds]] = [255, 0, 0]
+out_img[nonzeroy[right_lane_inds], nonzerox[right_lane_inds]] = [100, 200, 255]
+plt.imshow(out_img)
+plt.plot(left_fitx, ploty, color='yellow')
+plt.plot(right_fitx, ploty, color='yellow')
+plt.xlim(0, 1280)
+plt.ylim(720, 0)
+
+print('...')
 
 
 
