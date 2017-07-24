c3123c8c48dcc146b952674b8ac92d012caef9be
abhitrip <atripath@eng.ucsd.edu>
2017-07-24 02:57:12 -0700

    Adding final changes

--- lane_pipeline_inter.py
+++ lane_pipeline_inter.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 import collections
-
+from LaneLine import Line
 # For Displaying/Viewing/Editing video on Ipython console
 from moviepy.editor import VideoFileClip
 from IPython.display import HTML
@@ -110,9 +110,9 @@ def sobelMagnitude(img, thresh_min=75, thresh_max=255, sobel_kernel=11):
 
 
 def hsvThres(img, thresh_min , thresh_max ):
-    hls = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
-    mask_gt = np.all(hls>thresh_min, axis=2)
-    mask_lt = np.all(hls<thresh_max, axis=2)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask_gt = np.all(hsv>thresh_min, axis=2)
+    mask_lt = np.all(hsv<thresh_max, axis=2)
     hsv_mask = np.logical_and(mask_gt, mask_lt)
     return hsv_mask
 
@@ -174,8 +174,9 @@ def binariseImage(img, displayImages=True):
         plt.show()
     return closing
 
-test_images = glob.glob('test_images/*.jpg')
 """
+test_images = glob.glob('test_images/*.jpg')
+
 img = cv2.imread("test_images/test2.jpg")
 closed = binariseImage(img, True)
 """
@@ -211,11 +212,349 @@ def perspectiveTransform(img, displayImages=True, showQuad=True):
 
 # show result on test images
 # for test_img in glob.glob('test_images/*.jpg'):
+"""
 test_img = "test_images/test4.jpg"
 img = mpimg.imread(test_img)
 img_undistorted = undistort(img, mtx, dist)
 img_binary = binariseImage(img_undistorted,displayImages=False)
-img_birdeye, M, Minv = perspectiveTransform(img_undistorted, True)
+img_birdeye, M, Minv = perspectiveTransform(img_binary, True)
+print(img_binary.shape)
+"""
+## Now do the sliding window fitting part
+
+# Define method to fit polynomial to binary image with lines extracted, using sliding window
+ym_per_pix = 30 / 720   # meters per pixel in y dimension
+xm_per_pix = 3.7 / 700  # meters per pixel in x dimension
+time_window = 15        # results are averaged over this number of frames
+def sliding_windows(birdeye_binary, line_lt, line_rt, n_windows=9, print_image=False):
+    height, width = birdeye_binary.shape
+
+    # Assuming you have created a warped binary image called "binary_warped"
+    # Take a histogram of the bottom half of the image
+    histogram = np.sum(birdeye_binary[height//2:-30, :], axis=0)
+
+    # Create an output image to draw on and  visualize the result
+    out_img = np.dstack((birdeye_binary, birdeye_binary, birdeye_binary)) * 255
+
+    # Find the peak of the left and right halves of the histogram
+    # These will be the starting point for the left and right lines
+    midpoint = len(histogram) // 2
+    leftx_base = np.argmax(histogram[:midpoint])
+    rightx_base = np.argmax(histogram[midpoint:]) + midpoint
+
+    # Set height of windows
+    window_height = np.int(height / n_windows)
+
+    # Identify the x and y positions of all nonzero pixels in the image
+    nonzero = birdeye_binary.nonzero()
+    nonzero_y = np.array(nonzero[0])
+    nonzero_x = np.array(nonzero[1])
+
+    # Current positions to be updated for each window
+    leftx_current = leftx_base
+    rightx_current = rightx_base
+
+    margin = 100  # width of the windows +/- margin
+    minpix = 50   # minimum number of pixels found to recenter window
+
+    # Create empty lists to receive left and right lane pixel indices
+    left_lane_inds = []
+    right_lane_inds = []
+
+    # Step through the windows one by one
+    for window in range(n_windows):
+        # Identify window boundaries in x and y (and right and left)
+        win_y_low = height - (window + 1) * window_height
+        win_y_high = height - window * window_height
+        win_xleft_low = leftx_current - margin
+        win_xleft_high = leftx_current + margin
+        win_xright_low = rightx_current - margin
+        win_xright_high = rightx_current + margin
+
+        # Draw the windows on the visualization image
+        cv2.rectangle(out_img, (win_xleft_low, win_y_low), (win_xleft_high, win_y_high), (0, 255, 0), 2)
+        cv2.rectangle(out_img, (win_xright_low, win_y_low), (win_xright_high, win_y_high), (0, 255, 0), 2)
+
+        # Identify the nonzero pixels in x and y within the window
+        good_left_inds = ((nonzero_y >= win_y_low) & (nonzero_y < win_y_high) & (nonzero_x >= win_xleft_low)
+                          & (nonzero_x < win_xleft_high)).nonzero()[0]
+        good_right_inds = ((nonzero_y >= win_y_low) & (nonzero_y < win_y_high) & (nonzero_x >= win_xright_low)
+                           & (nonzero_x < win_xright_high)).nonzero()[0]
+
+        # Append these indices to the lists
+        left_lane_inds.append(good_left_inds)
+        right_lane_inds.append(good_right_inds)
+
+        # If you found > minpix pixels, recenter next window on their mean position
+        if len(good_left_inds) > minpix:
+            leftx_current = np.int(np.mean(nonzero_x[good_left_inds]))
+        if len(good_right_inds) > minpix:
+            rightx_current = np.int(np.mean(nonzero_x[good_right_inds]))
+
+    # Concatenate the arrays of indices
+    left_lane_inds = np.concatenate(left_lane_inds)
+    right_lane_inds = np.concatenate(right_lane_inds)
+
+    # Extract left and right line pixel positions
+    line_lt.allx, line_lt.ally = nonzero_x[left_lane_inds], nonzero_y[left_lane_inds]
+    line_rt.allx, line_rt.ally = nonzero_x[right_lane_inds], nonzero_y[right_lane_inds]
+
+    detected = True
+    if not list(line_lt.allx) or not list(line_lt.ally):
+        left_fit_pixel = line_lt.lastFitPixel
+        left_fit_meter = line_lt.lastFitMetPerPixel
+        detected = False
+    else:
+        left_fit_pixel = np.polyfit(line_lt.ally, line_lt.allx, 2)
+        left_fit_meter = np.polyfit(line_lt.ally * ym_per_pix, line_lt.allx * xm_per_pix, 2)
+
+    if not list(line_rt.allx) or not list(line_rt.ally):
+        right_fit_pixel = line_rt.last_fit_pixel
+        right_fit_meter = line_rt.last_fit_meter
+        detected = False
+    else:
+        right_fit_pixel = np.polyfit(line_rt.ally, line_rt.allx, 2)
+        right_fit_meter = np.polyfit(line_rt.ally * ym_per_pix, line_rt.allx * xm_per_pix, 2)
+
+    line_lt.update_line_eq(left_fit_pixel, left_fit_meter, detected=detected)
+    line_rt.update_line_eq(right_fit_pixel, right_fit_meter, detected=detected)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, height - 1, height)
+    left_fitx = left_fit_pixel[0] * ploty ** 2 + left_fit_pixel[1] * ploty + left_fit_pixel[2]
+    right_fitx = right_fit_pixel[0] * ploty ** 2 + right_fit_pixel[1] * ploty + right_fit_pixel[2]
+
+    out_img[nonzero_y[left_lane_inds], nonzero_x[left_lane_inds]] = [255, 0, 0]
+    out_img[nonzero_y[right_lane_inds], nonzero_x[right_lane_inds]] = [0, 0, 255]
+
+    if print_image:
+        f, ax = plt.subplots(1, 2, figsize=(15,6),  subplot_kw={'xticks': [], 'yticks': []})
+        f.set_facecolor('white')
+        ax[0].imshow(birdeye_binary, cmap='gray')
+        ax[1].imshow(out_img)
+        ax[1].plot(left_fitx, ploty, color='yellow')
+        ax[1].plot(right_fitx, ploty, color='yellow')
+        ax[1].set_xlim(0, 1280)
+        ax[1].set_ylim(720, 0)
+
+        plt.show()
+
+    return line_lt, line_rt, out_img
+
+# visualize the result on example image
+def approx_by_previous_fits(birdeye_binary, line_lt, line_rt, print_image=False):
+    """
+    This function uses previously detected lane-lines to
+    search of lane-lines in the current frame.
+    """
+
+    height, width = birdeye_binary.shape
+
+    left_fit_pixel = line_lt.lastFitPixel
+    right_fit_pixel = line_rt.lastFitPixel
+
+    nonzero = birdeye_binary.nonzero()
+    nonzero_y = np.array(nonzero[0])
+    nonzero_x = np.array(nonzero[1])
+    margin = 100
+    left_lane_inds = (
+    (nonzero_x > (left_fit_pixel[0] * (nonzero_y ** 2) + left_fit_pixel[1] * nonzero_y + left_fit_pixel[2] - margin)) & (
+    nonzero_x < (left_fit_pixel[0] * (nonzero_y ** 2) + left_fit_pixel[1] * nonzero_y + left_fit_pixel[2] + margin)))
+    right_lane_inds = (
+    (nonzero_x > (right_fit_pixel[0] * (nonzero_y ** 2) + right_fit_pixel[1] * nonzero_y + right_fit_pixel[2] - margin)) & (
+    nonzero_x < (right_fit_pixel[0] * (nonzero_y ** 2) + right_fit_pixel[1] * nonzero_y + right_fit_pixel[2] + margin)))
+
+    # Extract left and right line pixel positions
+    line_lt.allx, line_lt.ally = nonzero_x[left_lane_inds], nonzero_y[left_lane_inds]
+    line_rt.allx, line_rt.ally = nonzero_x[right_lane_inds], nonzero_y[right_lane_inds]
+
+    detected = True
+    if not list(line_lt.allx) or not list(line_lt.ally):
+        left_fit_pixel = line_lt.lastFitPixel
+        left_fit_meter = line_lt.lastFitMetPerPixel
+        detected = False
+    else:
+        left_fit_pixel = np.polyfit(line_lt.ally, line_lt.allx, 2)
+        left_fit_meter = np.polyfit(line_lt.ally * ym_per_pix, line_lt.allx * xm_per_pix, 2)
+
+    if not list(line_rt.allx) or not list(line_rt.ally):
+        right_fit_pixel = line_rt.lastFitPixel
+        right_fit_meter = line_rt.lastFitMetPerPixel
+        detected = False
+    else:
+        right_fit_pixel = np.polyfit(line_rt.ally, line_rt.allx, 2)
+        right_fit_meter = np.polyfit(line_rt.ally * ym_per_pix, line_rt.allx * xm_per_pix, 2)
+
+    line_lt.update_line_eq(left_fit_pixel, left_fit_meter, detected=detected)
+    line_rt.update_line_eq(right_fit_pixel, right_fit_meter, detected=detected)
+
+    # Generate x and y values for plotting
+    ploty = np.linspace(0, height - 1, height)
+    left_fitx = left_fit_pixel[0] * ploty ** 2 + left_fit_pixel[1] * ploty + left_fit_pixel[2]
+    right_fitx = right_fit_pixel[0] * ploty ** 2 + right_fit_pixel[1] * ploty + right_fit_pixel[2]
+
+    # Create an image to draw on and an image to show the selection window
+    img_fit = np.dstack((birdeye_binary, birdeye_binary, birdeye_binary)) * 255
+    window_img = np.zeros_like(img_fit)
+
+    # Color in left and right line pixels
+    img_fit[nonzero_y[left_lane_inds], nonzero_x[left_lane_inds]] = [255, 0, 0]
+    img_fit[nonzero_y[right_lane_inds], nonzero_x[right_lane_inds]] = [0, 0, 255]
+
+    # Generate a polygon to illustrate the search window area
+    # And recast the x and y points into usable format for cv2.fillPoly()
+    left_line_window1 = np.array([np.transpose(np.vstack([left_fitx - margin, ploty]))])
+    left_line_window2 = np.array([np.flipud(np.transpose(np.vstack([left_fitx + margin, ploty])))])
+    left_line_pts = np.hstack((left_line_window1, left_line_window2))
+    right_line_window1 = np.array([np.transpose(np.vstack([right_fitx - margin, ploty]))])
+    right_line_window2 = np.array([np.flipud(np.transpose(np.vstack([right_fitx + margin, ploty])))])
+    right_line_pts = np.hstack((right_line_window1, right_line_window2))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(window_img, np.int_([left_line_pts]), (0, 255, 0))
+    cv2.fillPoly(window_img, np.int_([right_line_pts]), (0, 255, 0))
+    result = cv2.addWeighted(img_fit, 1, window_img, 0.3, 0)
+
+    if print_image:
+        plt.imshow(result)
+        plt.plot(left_fitx, ploty, color='yellow')
+        plt.plot(right_fitx, ploty, color='yellow')
+        plt.xlim(0, 1280)
+        plt.ylim(720, 0)
+
+        plt.show()
+
+    return line_lt, line_rt, img_fit
+
+line_lt, line_rt = Line(frameBuffLen=10), Line(frameBuffLen=10)
+img = cv2.imread("test_images/test5.jpg")
+img_undistorted = undistort(img, mtx, dist)
+img_binary = binariseImage(img_undistorted)
+img_birdeye, M, Minv = perspectiveTransform(img_binary)
+line_lt, line_rt, img_out = sliding_windows(img_birdeye, line_lt, line_rt, n_windows=7, print_image=True)
+
+def combine_line_with_road(img_undistorted, Minv, leftLine, rightLine, keepCurr):
+    """
+    Combine into 1 image, the driving boundary, lane lines and original image
+    """
+    height,width = img_undistorted.shape[:2]
+    left_fit = leftLine.avgFit if keepCurr else leftLine.lastFitPixel
+    right_fit = rightLine.avgFit if keepCurr else rightLine.lastFitPixel
+
+    # Now get x and y co-ords for plotting
+    ploty = np.linspace(0, height-1, height)
+    left_fitx = left_fit[0] * ploty ** 2 + left_fit[1] * ploty + left_fit[2]
+    right_fitx = right_fit[0] * ploty ** 2 + right_fit[1] * ploty + right_fit[2]
+    # draw road as green polygon on original frame
+    road_warp = np.zeros_like(img_undistorted, dtype=np.uint8)
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+    cv2.fillPoly(road_warp, np.int_([pts]), (0, 255, 0))
+    road_dewarped = cv2.warpPerspective(road_warp, Minv, (width, height))  # Warp back to original image space
+
+    combined_img = cv2.addWeighted(img_undistorted, 1., road_dewarped, 0.3, 0)
+
+    # now separately draw solid lines to highlight them
+    line_warp = np.zeros_like(img_undistorted)
+    line_warp = line_lt.draw_on_img(line_warp, colour=(255, 0, 0), avg=keepCurr)
+    line_warp = line_rt.draw_on_img(line_warp, colour=(255, 0, 0), avg=keepCurr)
+    line_dewarped = cv2.warpPerspective(line_warp, Minv, (width, height))
+
+    lines_mask = combined_img.copy()
+    idx = np.any([line_dewarped != 0][0], axis=2)
+    lines_mask[idx] = line_dewarped[idx]
+
+    combined_img = cv2.addWeighted(src1=lines_mask, alpha=0.8, src2=combined_img, beta=0.5, gamma=0.)
+
+    return combined_img
+
+def add_metrics_to_img(combined_img, img_binary, img_birdeye, img_fit, line_lt, line_rt, offset_meter):
+    """
+   Print the final image with radius of curvature.
+    """
+    h, w = combined_img.shape[:2]
+
+    thumb_ratio = 0.2
+    thumb_h, thumb_w = int(thumb_ratio * h), int(thumb_ratio * w)
+
+    off_x, off_y = 20, 15
+
+    # add a gray rectangle to highlight the upper area
+    mask = combined_img.copy()
+    mask = cv2.rectangle(mask, pt1=(0, 0), pt2=(500, thumb_h+2*off_y), colourr=(0, 0, 0), thickness=cv2.FILLED)
+    combined_img = cv2.addWeighted(src1=mask, alpha=0.2, src2=combined_img, beta=0.8, gamma=0)
+
+    # add text (curvature and offset info) on the upper right of the blend
+    mean_curvature_meter = np.mean([line_lt.curvatureMetPerPixel, line_rt.curvatureMetPerPixel])
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(combined_img, 'Radius of curvature: {:.02f}m'.format(mean_curvature_meter), (30, 60), font, 0.9, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(combined_img, 'Offset from center: {:.02f}m'.format(offset_meter), (30, 130), font, 0.9, (255, 255, 255), 2, cv2.LINE_AA)
+
+    return combined_img
+
+def get_offset_from_center(line_lt, line_rt, frame_width):
+    """
+    Compute offset from center of the inferred lane.
+    The offset from the lane center can be computed under the hypothesis that the camera is fixed
+    and mounted in the midpoint of the car roof. In this case, we can approximate the car's deviation
+    from the lane center as the distance between the center of the image and the midpoint at the bottom
+    of the image of the two lane-lines detected.
+    """
+    if line_lt.detected and line_rt.detected:
+        line_lt_bottom = np.mean(line_lt.allx[line_lt.ally > 0.95 * line_lt.ally.max()])
+        line_rt_bottom = np.mean(line_rt.allx[line_rt.ally > 0.95 * line_rt.ally.max()])
+        lane_width = line_rt_bottom - line_lt_bottom
+        midpoint = frame_width / 2
+        offset_pix = abs((line_lt_bottom + lane_width / 2) - midpoint)
+        offset_meter = xm_per_pix * offset_pix
+    else:
+        offset_meter = -1
+
+    return offset_meter
+
+def process_pipeline(frame, keepCurr=True):
+    """
+    Apply the whole LD pipeline to a single frame
+    """
+    global leftLine,rightLine,processedFrames
+    # First remove distortion of image frame
+    img_undistorted = undistort(frame,mtx,dist)
+    # Now binarise image
+    img_binary = binariseImage(img,False)
+    # Now Compute Perspective Transform
+    img_birdeye, M, Minv = perspectiveTransform(img_binary)
+    # fit 2-degree polynomial curve onto lane lines found
+    if processedFrames>0 and keepCurr and leftLine.detected and rightLine.detected:
+        leftLine, rightLine, img_fit = approx_by_previous_fits(img_birdeye, leftLine, rightLine)
+    else:
+        leftLine, rightLine, img_fit = sliding_windows(img_birdeye, leftLine, rightLine)
+
+    # Compute offset in meter from ctr of frame
+    offset = get_offset_from_center(leftLine,rightLine,frame_width=frame.shape[1])
+
+    # draw the surface enclosed by lane lines back onto the original frame
+    combined_img = combine_line_with_road(img_undistorted, Minv, leftLine, rightLine, keepCurr)
+
+    # stitch on the top of final output images from different steps of the pipeline
+    blend_output = add_metrics_to_img(combined_img, img_binary, img_birdeye, img_fit, leftLine, rightLine, offset)
+
+    processedFrames+=1
+    return cv2.cvtColor(blend_output, cv2.COLOR_BGR2RGB)
+
+images = glob.glob(os.path.join("test_images", '*.jpg'))
+"""
+for filename in images:
+    img = mpimg.imread(filename)
+    blend = process_pipeline(img, keepCurr=False)
+    cv2.imwrite("output_" + filename, blend)
+"""
+processedFrames = 0
+leftLine, rightLine = Line(frameBuffLen=10), Line(frameBuffLen=10)
+frame = cv2.imread("test_images/test5.jpg")
+blend = process_pipeline(frame, keepCurr=False)
+
+
 
 
 
--- writeup_template.md
+++ writeup_template.md
@@ -1,10 +1,6 @@
-## Writeup Template
 
-### You can use this file as a template for your writeup if you want to submit it as a markdown file, but feel free to use some other method and submit a pdf if you prefer.
 
----
-
-**Advanced Lane Finding Project**
+# **Advanced Lane Finding Project**
 
 The goals / steps of this project are the following:
 
@@ -19,19 +15,19 @@ The goals / steps of this project are the following:
 
 [//]: # (Image References)
 
-[image1]: ./examples/undistort_output.png "Undistorted"
-[image2]: ./test_images/test1.jpg "Road Transformed"
-[image3]: ./examples/binary_combo_example.jpg "Binary Example"
-[image4]: ./examples/warped_straight_lines.jpg "Warp Example"
-[image5]: ./examples/color_fit_lines.jpg "Fit Visual"
-[image6]: ./examples/example_output.jpg "Output"
-[video1]: ./project_video.mp4 "Video"
+[image1]: ./examples/undistort_output.png 
+[image2]: ./test_images/test2_transformed.jpg 
+[image3]: ./test_images/test2_bin.jpg 
+[image4]: ./test4_persp.jpg 
+[image5]: ./test_images/sliding.jpg 
+[image6]: ./test_images/road_detected.jpg 
+[video1]: ./project_video_output.mp4 
 
 ## [Rubric](https://review.udacity.com/#!/rubrics/571/view) Points
 
-### Here I will consider the rubric points individually and describe how I addressed each point in my implementation.  
+Here I will consider the rubric points individually and describe how I addressed each point in my implementation.  
 
----
+
 
 ### Writeup / README
 
@@ -43,7 +39,7 @@ You're reading it!
 
 #### 1. Briefly state how you computed the camera matrix and distortion coefficients. Provide an example of a distortion corrected calibration image.
 
-The code for this step is contained in the first code cell of the IPython notebook located in "./examples/example.ipynb" (or in lines # through # of the file called `some_file.py`).  
+The code for this step is contained in the second code cell of the IPython notebook located in "./advLaneFinding.ipynb" (Step 1. Camera Calibration headlined cell).  
 
 I start by preparing "object points", which will be the (x, y, z) coordinates of the chessboard corners in the world. Here I am assuming the chessboard is fixed on the (x, y) plane at z=0, such that the object points are the same for each calibration image.  Thus, `objp` is just a replicated array of coordinates, and `objpoints` will be appended with a copy of it every time I successfully detect all chessboard corners in a test image.  `imgpoints` will be appended with the (x, y) pixel position of each of the corners in the image plane with each successful chessboard detection.  
 
@@ -60,35 +56,37 @@ To demonstrate this step, I will describe how I apply the distortion correction 
 
 #### 2. Describe how (and identify where in your code) you used color transforms, gradients or other methods to create a thresholded binary image.  Provide an example of a binary image result.
 
-I used a combination of color and gradient thresholds to generate a binary image (thresholding steps at lines # through # in `another_file.py`).  Here's an example of my output for this step.  (note: this is not actually from one of the test images)
+I used a combination of color and gradient thresholds to generate a binary image. To detect white lines , histogram equalisation is enough. However, inorder to detect yellow lines, I used the v-channel of image in hsv colour space. Finally, a sobel mask is used to compute the gradients. Also, I used 
+morphological dilation and erosion to make the edge lines continuous. The code is shown in code cell - **2. Image Image preprocessing, thresholding and binarisation**
+
 
 ![alt text][image3]
 
 #### 3. Describe how (and identify where in your code) you performed a perspective transform and provide an example of a transformed image.
 
-The code for my perspective transform includes a function called `warper()`, which appears in lines 1 through 8 in the file `example.py` (output_images/examples/example.py) (or, for example, in the 3rd code cell of the IPython notebook).  The `warper()` function takes as inputs an image (`img`), as well as source (`src`) and destination (`dst`) points.  I chose the hardcode the source and destination points in the following manner:
+The code for my perspective transform includes a function called `perspectiveTransform()`, which appears in the `Perspective transform` cell  of the IPython notebook.  The `perspectiveTransform()` function takes as inputs an image (`img`), as well as source (`src`) and destination (`dst`) points.  I chose the hardcode the source and destination points in the following manner:
 
 ```python
 src = np.float32(
-    [[(img_size[0] / 2) - 55, img_size[1] / 2 + 100],
-    [((img_size[0] / 6) - 10), img_size[1]],
-    [(img_size[0] * 5 / 6) + 60, img_size[1]],
-    [(img_size[0] / 2 + 55), img_size[1] / 2 + 100]])
+    [[0, img_size[0] -30],
+    [img_size[1] ,img_size[0] -30] ],
+    [540, 450],
+    [750,450])
 dst = np.float32(
-    [[(img_size[0] / 4), 0],
-    [(img_size[0] / 4), img_size[1]],
-    [(img_size[0] * 3 / 4), img_size[1]],
-    [(img_size[0] * 3 / 4), 0]])
+    [[0,img_size[0]],
+    [img_size[1], img_size[0]],
+    [0, 0],
+    [img_size[1], 0]])
 ```
 
 This resulted in the following source and destination points:
 
 | Source        | Destination   | 
 |:-------------:|:-------------:| 
-| 585, 460      | 320, 0        | 
-| 203, 720      | 320, 720      |
-| 1127, 720     | 960, 720      |
-| 695, 460      | 960, 0        |
+| 0, 690        | 0, 720        | 
+| 1280, 690     | 1280, 720     |
+| 540, 450      |  0, 0         |
+| 1280, 450     | 1280, 0       |
 
 I verified that my perspective transform was working as expected by drawing the `src` and `dst` points onto a test image and its warped counterpart to verify that the lines appear parallel in the warped image.
 
@@ -96,17 +94,21 @@ I verified that my perspective transform was working as expected by drawing the 
 
 #### 4. Describe how (and identify where in your code) you identified lane-line pixels and fit their positions with a polynomial?
 
-Then I did some other stuff and fit my lane lines with a 2nd order polynomial kinda like this:
+I used the same algorithm as mentioned in the video lectures. First, check the histogram of the lower half of the image and find the two peaks for the left and right lines. Then we use the sliding window method to work our way upwards and find the relevant points in the image which mark the lane. Next, Use the np.polyfit() method to fit a second degree polynomial to these points.  You can see the code in sliding_windows() function.
+For videos, we can reuse the previously detected lane lines(of previous n frames) to get an approximation of lane line in current frame. You can see the code in  approx_by_previous_fits().
 
 ![alt text][image5]
 
 #### 5. Describe how (and identify where in your code) you calculated the radius of curvature of the lane and the position of the vehicle with respect to center.
 
-I did this in lines # through # in my code in `my_other_file.py`
+The offset from the lane center can be computed assuming that the camera is fixed and mounted in the midpoint of the car roof. In this case, we can approximate the car's deviation from the lane center as the distance between the center of the image and the midpoint at the bottom of the image of the two lane-lines detected. This is done using the method compute_offset_from_center().
+The radius of curvature can be computed using the coefficients of 2nd degree polynomial fitted to the lane lines. The code is in the method curvature_meter().
+
+
 
 #### 6. Provide an example image of your result plotted back down onto the road such that the lane area is identified clearly.
 
-I implemented this step in lines # through # in my code in `yet_another_file.py` in the function `map_lane()`.  Here is an example of my result on a test image:
+The methods blend_with_road() and print_final_frame() are used to draw the lane back onto the road.  Here is an example of my result on a test image:
 
 ![alt text][image6]
 
@@ -116,12 +118,18 @@ I implemented this step in lines # through # in my code in `yet_another_file.py`
 
 #### 1. Provide a link to your final video output.  Your pipeline should perform reasonably well on the entire project video (wobbly lines are ok but no catastrophic failures that would cause the car to drive off the road!).
 
-Here's a [link to my video result](./project_video.mp4)
+Here's a [link to my video result](./project_video_output.mp4)
 
 ---
 
 ### Discussion
 
-#### 1. Briefly discuss any problems / issues you faced in your implementation of this project.  Where will your pipeline likely fail?  What could you do to make it more robust?
+#### 1. Briefly discuss any problems / issues you faced in your implementation of this project.  Where will your pipeline likely fail?  What could you do to make it more robust ?
 
-Here I'll talk about the approach I took, what techniques I used, what worked and why, where the pipeline might fail and how I might improve it if I were going to pursue this project further.  
+The main challenge was to select optimum parameters for the gradient thresholds. It is hard, since thresholds were chosen based on the 6 test images, but they might not work for all images. In other words, a generic threshold parameter setting is almost impossible. 
+Moreover, the pipeline is still not full proof to changes in brightness, contrast etc. 
+
+To make the pipeline more robust:-
+1. Use Dynamic thersholding i.e. considering separate threshold parameters for different horizontal sections of the image. 
+2. Modifying our algorithm to use  condifence levels for fits. It means we can reject fits, whose confidence is below a threshold. For example, If either of left or right fit is not meeting the confidence, then we could use the previous fit.
+
